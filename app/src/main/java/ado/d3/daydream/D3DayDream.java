d3a00cae68ab31193fa4d0ccdd235ce2486e1a9f
Jonathan Querubina <jotadev@me.com>
2016-05-27 11:13:14 -0300

    Block major buttons

--- app/src/main/java/ado/d3/daydream/D3DayDream.java
+++ app/src/main/java/ado/d3/daydream/D3DayDream.java
@@ -5,31 +5,59 @@ import android.widget.VideoView;
 import android.net.Uri;
 import android.util.Log;
 import android.media.MediaPlayer;
+import android.content.Intent;
+import android.view.KeyEvent;
+import android.widget.Button;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by john on 5/23/16.
  */
+
 public class D3DayDream extends DreamService {
 
-    VideoView videoHolder;
+    private VideoView videoHolder;
+    private final List blockedKeys = new ArrayList(Arrays.asList(KeyEvent.KEYCODE_VOLUME_DOWN, KeyEvent.KEYCODE_VOLUME_UP, KeyEvent.KEYCODE_BACK, KeyEvent.KEYCODE_HOME, KeyEvent.KEYCODE_POWER));
+    private Button hiddenExitButton;
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if(!hasFocus) {
+            // Close every kind of system dialog
+            Intent closeDialog = new Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS);
+            sendBroadcast(closeDialog);
+        }
+    }
+
+    @Override
+    public boolean dispatchKeyEvent(KeyEvent event) {
+        Log.v("D3DayDreamLog", "keyPressed");
+        if (blockedKeys.contains(event.getKeyCode())) {
+            return true;
+        } else {
+            return super.dispatchKeyEvent(event);
+        }
+    }
 
     @Override
     public void onDreamingStarted() {
         super.onDreamingStarted();
-        Log.v("D3DayDream", "started");
-
+        Log.v("D3DayDreamLog", "started");
     }
 
     @Override
     public void onDreamingStopped(){
         super.onDreamingStopped();
-        Log.v("D3DayDream", "stoped");
+        Log.v("D3DayDreamLog", "stoped");
     }
 
     @Override
     public void onAttachedToWindow() {
         super.onAttachedToWindow();
-        Log.v("D3DayDream", "attached");
+        Log.v("D3DayDreamLog", "attached");
 
         setContentView(R.layout.main);
 
